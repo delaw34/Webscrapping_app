4ec8ee35e119bef1ac1d8744ec7b260c02832797
Lawrence Akpan <delaw34@yahoo.com>
2023-05-30 19:23:19 +0100

    Update scrapping.py

--- scrapping.py
+++ scrapping.py
@@ -1,48 +1,26 @@
-# import necessary modules (re, requests, os, sys) 
 import os
-import sys
 import re
 import requests
 
-#function takes a text input and uses regular expressions (re.findall) to extract phone numbers
-
-def extract_phone_numbers(text):
-    pattern = r"234[789][01]\d{8}\b" 
-    # pattern = r"^\+?\d{1,3}[-.\s]?\(?\d{1,4}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,9}$"
-    phone_numbers = re.findall(pattern, text)
-    return phone_numbers
-
-# function takes a text input and uses regular expressions to extract email addresses. The regular expression pattern
-
-def extract_emails(text):
-    pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
-    emails = re.findall(pattern, text)
-    return emails
-
-#function takes a text input and uses regular expressions to extract HTML links. The regular expression pattern
-def extract_links(text):
-    pattern = r'<a\s+(?:[^>]*)?href="([^"]*)"'
-    links = re.findall(pattern, text)
-    return links
+def extract_pattern(text, pattern):
+    return re.findall(pattern, text)
 
 def save_to_file(data, filename):
-    with open(os.path.join(sys.path[0], filename), 'w') as file:
-        for item in data:
-            file.write(item + '\n')
-#function takes data and a url as input. 
+    file_path = input(f"Enter the file path to save '{filename}': ")
+    with open(file_path, 'w') as file:
+        file.write('\n'.join(data))
+    return file_path
+
 def main():
     url = input("Enter the website URL: ")
-    
-#this improve functionality and error handling of the code.
     try:
-#It opens the file with the specified filename and writes each item in the data list to the file, separated by a newline.
         response = requests.get(url)
         response.raise_for_status()  # Check for any HTTP errors
         html_content = response.text
 
-        phone_numbers = extract_phone_numbers(html_content)
-        emails = extract_emails(html_content)
-        links = extract_links(html_content)
+        phone_numbers = extract_pattern(html_content, r"234[789][01]\d{8}\b")
+        emails = extract_pattern(html_content, r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b")
+        links = extract_pattern(html_content,  r'<a\s+(?:[^>])?href="([^"])"')
 
         save_to_file(phone_numbers, 'phone_numbers.txt')
         save_to_file(emails, 'emails.txt')
@@ -51,5 +29,5 @@ def main():
         print("Extraction complete. Phone numbers, emails, and links saved to separate files.")
     except requests.exceptions.RequestException as e:
         print(f"Error occurred while retrieving the webpage: {e}")
-        
+
 main()
